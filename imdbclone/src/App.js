db93bfcb9c8258a0c0720b907233635e89361ee4
Ashish Bhardwaj <erasish41@gmail.com>
2023-12-12 11:54:54 +0530

    address added

--- imdbclone/src/App.js
+++ imdbclone/src/App.js
@@ -1,10 +1,19 @@
 import React from 'react'
-import Home from './Home'
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Home from './Home';
+import SingleMovie from './SingleMovie'
+import Error from './Error'
 
 const App = () => {
   return (
     <>
-      <Home></Home>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="movie/:id" element={<SingleMovie/>}/>
+          <Route path="*" element={<Error/>}/>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
